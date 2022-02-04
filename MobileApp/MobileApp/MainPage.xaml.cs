0ef979544ae76b54567b1f35278da2bf4e2c3d04
201923 <201923@1C1B0D632E501>
2022-02-04 09:45:06 +0300

    Добавьте файлы проекта.

---
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        private async void btnSignUpClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
+        }
+
+        private async void btnLoginClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProjectsPage());
+        }
+    }
+}
